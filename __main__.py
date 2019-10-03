b1ec9669ef0ba77a0b5f6b7bf67180222ea6b953
Louis Annabi <louis.annabi@gmail.com>
2019-10-03 08:50:21 +0200

    added random_in_area initial position

--- __main__.py
+++ __main__.py
@@ -24,27 +24,30 @@ import flatland.agents.agent as agent
 from flatland.default_parameters.agents import *
 
 agent_params = {
-    'frame' : {
+    'frame': {
         'type': 'forward_head',
-        'params' : {
+        'params': {
             'base_radius': 15,
                 }
     },
-    'controller' :{
-        'type': 'random'
+    'controller': {
+        'type': 'keyboard'
     },
-    'sensors':{
+    'sensors': {
         #'rgb_1': {**rgb_default, **{'bodyAnchor': 'head', 'fovResolution': 128, 'fovRange': 1000} },
-        'rgb_2': {**rgb_default, **{'bodyAnchor': 'head', 'fovResolution': 64, 'fovRange': 1000} },
+        'rgb_2': {**rgb_default, 'bodyAnchor': 'head', 'fovResolution': 64, 'fovRange': 1000 },
         'touch_1' : touch_default,
     },
-    'starting_position':{
-        'type': 'fixed',
-        'coordinates' : [50, 50, 0]
+    'starting_position': {
+        'type': 'random_in_area',
+        'x_range': [25, 200],
+        'y_range': [25, 75],
+        'angle_range': [-1, 1]
     }
 }
 
 my_agent = agent.Agent(agent_params)
+
 
 ####################################################
 ####### Create game with playground and parameters
@@ -65,10 +68,10 @@ engine_parameters = {
 
 rules = {
     'replay_until_time_limit': True,
-    'time_limit' : 1000
+    'time_limit': 1000
 }
 
-game = Engine( playground = pg, agents = [my_agent], rules = rules, engine_parameters= engine_parameters )
+game = Engine(playground=pg, agents=[my_agent], rules=rules, engine_parameters=engine_parameters )
 
 
 clock = pygame.time.Clock()
@@ -80,23 +83,22 @@ while game.game_on:
     game.update_observations()
     game.set_actions()
     game.step()
-    #
-    # for agent in game.agents:
-    #
-    #     observations = agent.observations
-    #
-    #     for obs in observations:
-    #
-    #         im = np.asarray( observations[obs])
-    #         im = np.expand_dims(im, 0)
-    #         im = cv2.resize( im, (512, 50), interpolation=cv2.INTER_NEAREST )
-    #         cv2.imshow( obs, im )
-    #         cv2.waitKey(1)
-    #
-    # game.display_full_scene()
-    #
-    # print(game.time, my_agent.health)
-    #
-    # clock.tick(30)
+
+    for agent in game.agents:
+
+        observations = agent.observations
+
+        for obs in observations:
+
+            im = np.asarray( observations[obs])
+            im = np.expand_dims(im, 0)
+            im = cv2.resize( im, (512, 50), interpolation=cv2.INTER_NEAREST )
+            cv2.imshow( obs, im )
+            cv2.waitKey(1)
+        game.display_full_scene()
+
+    print(game.time, my_agent.health)
+
+    clock.tick(30)
 
 print( 1000/(time.time() - t1) )
--- flatland/utils/game_utils.py
+++ flatland/utils/game_utils.py
@@ -1,7 +1,22 @@
+from numpy import random
 
-def generate_position( position ):
+
+def generate_position(position):
 
     if position['type'] == 'fixed':
         pos = position['coordinates']
+
+    elif position['type'] == 'random_in_area':
+        min_x, max_x = position['x_range']
+        min_y, max_y = position['y_range']
+        min_angle, max_angle = position['angle_range']
+        pos = [
+            min_x + random.rand() * (max_x - min_x),
+            min_y + random.rand() * (max_y - min_y),
+            min_angle + random.rand() * (max_angle - min_angle),
+        ]
+
+    else:
+        raise ValueError('Initial position type not implemented: ' + position['type'])
 
     return pos
