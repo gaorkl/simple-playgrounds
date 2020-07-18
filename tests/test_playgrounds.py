bf6be2c05443eadab61fb1ec42d90e3df9b23d3f
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2020-07-18 10:22:54 +0100

    Corrected yaml deprecation

--- tests/test_playgrounds.py
+++ tests/test_playgrounds.py
@@ -3,22 +3,27 @@ from simple_playgrounds.controllers import Random
 from simple_playgrounds.entities.agents import BaseAgent
 from simple_playgrounds.game_engine import Engine
 
-agent = BaseAgent(controller=Random())
 
 # Add/remove agent from a playground
-playground = SingleRoom(size=(200, 200))
-playground.add_agent(agent)
-playground.remove_agent(agent)
-assert playground.agents == []
+def test_add_remove_agent():
+    agent = BaseAgent(controller=Random())
+    playground = SingleRoom(size=(200, 200))
+    playground.add_agent(agent)
+    playground.remove_agent(agent)
+    assert playground.agents == []
 
 # Create an engine then add an agent
-engine = Engine(playground, time_limit=100)
-playground.add_agent(agent)
-assert len(engine.agents) == 1
+def test_engine():
+    agent = BaseAgent(controller=Random())
+    playground = SingleRoom(size=(200, 200))
+    engine = Engine(playground, time_limit=100)
+    playground.add_agent(agent)
+    assert len(engine.agents) == 1
 
 # Run the playground, check that position changed
-pos_start = agent.position
-assert pos_start == (100., 100., 0.)
-engine.run()
-assert pos_start != agent.position
+def test_engine_run():
+    pos_start = agent.position
+    assert pos_start == (100., 100., 0.)
+    engine.run()
+    assert pos_start != agent.position
 
