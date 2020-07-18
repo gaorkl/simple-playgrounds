2cd52c3abfd69c56585d220b245caf54edff8a67
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2020-07-18 10:29:07 +0100

    Corrected tests

--- tests/test_playgrounds.py
+++ tests/test_playgrounds.py
@@ -3,12 +3,11 @@ from simple_playgrounds.controllers import Random
 from simple_playgrounds.entities.agents import BaseAgent
 from simple_playgrounds.game_engine import Engine
 
-playground = SingleRoom(size=(200, 200))
-agent = BaseAgent(controller=Random())
-
 
 # Add/remove agent from a playground
 def test_add_remove_agent():
+    playground = SingleRoom(size=(200, 200))
+    agent = BaseAgent(controller=Random())
     playground.add_agent(agent)
     playground.remove_agent(agent)
     assert playground.agents == []
@@ -16,13 +15,19 @@ def test_add_remove_agent():
 
 # Create an engine then add an agent
 def test_engine():
+    playground = SingleRoom(size=(200, 200))
+    agent = BaseAgent(controller=Random())
     engine = Engine(playground, time_limit=100)
     playground.add_agent(agent)
     assert len(engine.agents) == 1
+    playground.remove_agent(agent)
+    assert len(engine.agents) == 0
 
 
 # Run the playground, check that position changed
 def test_engine_run():
+    playground = SingleRoom(size=(200, 200))
+    agent = BaseAgent(controller=Random())
     engine = Engine(playground, agents=agent, time_limit=100)
     pos_start = agent.position
     assert pos_start == (100., 100., 0.)
