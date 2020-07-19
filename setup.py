df5844891a3fe1dc93bbee5ca737fb1096361fe4
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2020-07-19 22:44:02 +0100

    Added data (yaml) to repo

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_playgrounds',
-    version='0.9.9',
+    version='0.9.10',
     description='Simulator for AGI and RL',
     author='Michael Garcia Ortiz',
     author_email='michael.garcia-ortiz@city.ac.uk',
     packages=[package for package in find_packages()
-                if package.startswith('simple_playgrounds')]
+                if package.startswith('simple_playgrounds')],
+    include_package_data=True
 )
