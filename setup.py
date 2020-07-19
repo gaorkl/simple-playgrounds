e44dad719f6421007f5b71e8a565badbfbb7482d
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2020-07-19 09:27:11 +0100

    correcting init

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='simple_playgrounds',
-    version='0.9.7',
+    version='0.9.8',
     description='Simulator for AGI and RL',
     author='Michael Garcia Ortiz',
     author_email='michael.garcia-ortiz@city.ac.uk',
-    packages=['simple_playgrounds']
+    packages=[package for package in find_packages()
+                if package.startswith('simple_playgrounds')]
 )
