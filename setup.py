9680f1862b9e3108c3fd52784e04e8d726388f25
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2020-08-04 17:01:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='simple_playgrounds',
-    version='0.9.17',
+    version='0.9.18',
     description='Simulator for AGI and RL',
     author='Michael Garcia Ortiz',
     author_email='michael.garcia-ortiz@city.ac.uk',
