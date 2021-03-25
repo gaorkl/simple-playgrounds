b24053bb69717f2c9c60ff79b6d9a59f768778bd
Vince Jankovics <vincejankovics@gmail.com>
2021-03-25 17:09:26 +0000

    Agent tests refactored with pytest

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,23 +1,14 @@
-from simple_playgrounds.agents.controllers import Random
-from simple_playgrounds.agents.parts.platform import ForwardPlatform, FixedPlatform, \
-    HolonomicPlatform, ForwardBackwardPlatform, ForwardPlatformDiscrete
-from simple_playgrounds.agents.agents import BaseAgent, HeadAgent, HeadEyeAgent, TurretAgent
-from simple_playgrounds.playgrounds.empty import ConnectedRooms2D, SingleRoom
+import pytest
 
 from simple_playgrounds import Engine
-
-from simple_playgrounds.playground import PlaygroundRegister
-
+from simple_playgrounds.agents.controllers import Random
+from simple_playgrounds.playgrounds.empty import ConnectedRooms2D, SingleRoom
 from simple_playgrounds.utils.position_utils import CoordinateSampler
 
 
 def run_engine(agent, pg_class):
-
     playground = pg_class()
-
     playground.add_agent(agent)
-
-    print('Starting testing of ', pg_class.__name__)
 
     engine = Engine(playground, time_limit=1000)
     engine.run()
@@ -28,111 +19,29 @@ def run_engine(agent, pg_class):
     playground.remove_agent(agent)
 
 
-# Run all test playgrounds with BaseAgent
-def test_base_agent():
+def test_base_agent(is_interactive, platform_cls, pg_cls, agent_cls):
+    agent = agent_cls(controller=Random(),
+                      interactive=is_interactive,
+                      platform=platform_cls)
 
-    print('Testing of BaseAgent...')
-
-    for interactive in [True, False]:
-
-        if interactive:
-            print('.... with interactions')
-        else:
-            print('.... without interactions')
-
-        for platform in ForwardPlatform, FixedPlatform, HolonomicPlatform,\
-                        ForwardBackwardPlatform, ForwardPlatformDiscrete:
-
-            print('......... on platform' + str(platform))
-
-            agent = BaseAgent(controller=Random(), interactive=interactive, platform=platform)
-
-            for pg_name, pg_class in PlaygroundRegister.playgrounds['test'].items():
-                run_engine(agent, pg_class)
+    run_engine(agent, pg_cls)
 
 
-def test_headagent():
+@pytest.mark.parametrize(
+    "pg,limits",
+    [(SingleRoom((300, 300)), (300, 300)),
+     (ConnectedRooms2D((400, 400), (2, 2)), (200, 200))],
+)
+def test_agent_initial_position1(base_forward_agent, pg, limits):
+    agent = base_forward_agent
+    pg.add_agent(agent)
 
-    print('Testing of HeadAgent..............')
-
-    for interactive in [True, False]:
-
-        if interactive:
-            print('.... with interactions')
-        else:
-            print('.... without interactions')
-
-        for platform in ForwardPlatform, FixedPlatform, HolonomicPlatform, ForwardBackwardPlatform:
-
-            print('......... on platform' + str(platform))
-
-            agent = HeadAgent(controller=Random(), interactive=interactive, platform=platform)
-
-            for pg_name, pg_class in PlaygroundRegister.playgrounds['test'].items():
-                run_engine(agent, pg_class)
+    assert 0 < agent.position[0] < limits[0]
+    assert 0 < agent.position[1] < limits[1]
 
 
-def test_headeyeagent():
-
-    print('Testing of HeadEyeAgent..............')
-
-    for interactive in [True, False]:
-
-        if interactive:
-            print('.... with interactions')
-        else:
-            print('.... without interactions')
-
-        for platform in ForwardPlatform, FixedPlatform, HolonomicPlatform, ForwardBackwardPlatform:
-
-            print('......... on platform' + str(platform))
-
-            agent = HeadEyeAgent(controller=Random(), interactive=interactive, platform=platform)
-
-            for pg_name, pg_class in PlaygroundRegister.playgrounds['test'].items():
-                run_engine(agent, pg_class)
-
-
-def test_turretagent():
-
-    print('Testing of TurretAgent..............')
-
-    for interactive in [True, False]:
-
-        if interactive:
-            print('.... with interactions')
-        else:
-            print('.... without interactions')
-
-        agent = TurretAgent(controller=Random(), interactive=interactive)
-
-        for pg_name, pg_class in PlaygroundRegister.playgrounds['test'].items():
-            run_engine(agent, pg_class)
-
-
-def test_agent_initial_position():
-
-    print('Testing of intital position agent')
-
-    agent = BaseAgent(controller=Random(), interactive=False, platform=ForwardPlatform)
-
-    # Default Case
-    playground = SingleRoom( (300, 300))
-    playground.add_agent(agent)
-
-    assert 0 < agent.position[0] < playground.size[0]
-    assert 0 < agent.position[1] < playground.size[1]
-
-    playground.remove_agent(agent)
-
-    playground = ConnectedRooms2D((400, 400), (2, 2))
-    playground.add_agent(agent)
-
-    assert 0 < agent.position[0] < 200
-    assert 0 < agent.position[1] < 200
-
-    playground.remove_agent(agent)
-
+def test_agent_initial_position2(base_forward_agent):
+    agent = base_forward_agent
     # Modifying initial position in playground
     playground = SingleRoom((300, 300))
     playground.initial_agent_coordinates = ((50, 50), 0)
@@ -140,12 +49,14 @@ def test_agent_initial_position():
     assert agent.position == (50, 50)
     playground.remove_agent(agent)
 
+
+def test_agent_initial_position3(base_forward_agent):
+    agent = base_forward_agent
     # Setting initial position in playground as PositionAreaSampler
     playground = SingleRoom((300, 300))
     center, shape = playground.area_rooms[(0, 0)]
-    playground.initial_agent_coordinates = CoordinateSampler(center,
-                                                             area_shape='rectangle',
-                                                             width_length=shape)
+    playground.initial_agent_coordinates = CoordinateSampler(
+        center, area_shape='rectangle', width_length=shape)
     playground.add_agent(agent)
     pos_1 = agent.position
     playground.remove_agent(agent)
