da8fe76bfb85805b052643dd4495a29664478a6d
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2021-07-16 11:18:21 +0100

    Changed github actions for release.

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(name='simple_playgrounds',
-      version='0.9.30',
+      version='0.9.31',
       description='Simulator for Reinforcement Learning',
       author='Michael Garcia Ortiz',
       author_email='michael.garcia-ortiz@city.ac.uk',
       packages=find_packages(where="src"),
       package_dir={"": "src"},
       include_package_data=True,
-      install_requires=requirements)
+      install_requires=requirements,
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      )
