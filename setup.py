1fe59801f9712da6505b779ace5ae6e89a8168da
gaorkl <michael.garcia.ortiz@gmail.com>
2022-09-14 12:23:26 +0100

    version spg

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="spg",
-    version="0.9.31",
+    version="0.1.0",
     description="Simulator for Reinforcement Learning",
     author="Michael Garcia Ortiz",
     author_email="michael.garcia-ortiz@city.ac.uk",
