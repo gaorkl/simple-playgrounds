a862bbf49566a2b48e627b0fc64dd7bf86de8ea9
gaorkl <michael.garcia.ortiz@gmail.com>
2022-09-26 11:48:47 +0100

    Merged sensors and comms

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="spg",
-    version="0.1.0",
+    version="0.1.1",
     description="Simulator for Reinforcement Learning",
     author="Michael Garcia Ortiz",
     author_email="michael.garcia-ortiz@city.ac.uk",
