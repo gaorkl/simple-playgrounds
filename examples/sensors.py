6905a348fb3f50c116848145828d126d1866fb14
gaorkl <michael.garcia.ortiz@gmail.com>
2022-09-26 13:14:33 +0100

    Added internal sensors

---
+++ examples/sensors.py
@@ -0,0 +1,22 @@
+from spg.agent import HeadAgent
+from spg.element import Ball
+from spg.playground import WallClosedPG
+from spg.view import GUI
+
+playground = WallClosedPG(size=(500, 200), background=(23, 23, 21))
+
+ball = Ball()
+ball.graspable = True
+
+playground.add(ball, ((200, 0), 0))
+
+ball = Ball()
+ball.graspable = True
+
+playground.add(ball, ((200, 40), 0))
+
+agent = HeadAgent()
+playground.add(agent)
+
+gui = GUI(playground, agent, draw_sensors=True)
+gui.run()
